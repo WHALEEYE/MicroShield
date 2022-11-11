fabfc8eac9755763a13c3f287ede80b212e27d44
Isaac Jin <1094349973@qq.com>
2022-11-10 23:35:50 -0600

    feat(resolver): now support detect differences between policy sets

--- report-resolver/resolver.py
+++ report-resolver/resolver.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import os
 import time
@@ -21,6 +22,13 @@ def debug_info(info):
         print(f"\033[35m[DEBUG] {info}\033[0m")
 
 
+def label_matched(conditions, labels):
+    for key, value in conditions.items():
+        if key not in labels or labels[key] != value:
+            return False
+    return True
+
+
 class ResourceInfo:
 
     def __init__(self, resource_name, labels, namespace):
@@ -39,18 +47,20 @@ class Policy:
 
     def __init__(self, resource_name, inside_labels, namespace, resource_type):
         self.name = f"{resource_type.name.lower()}-{resource_name}-{namespace}"
-        self.template = templates.policy_template(self.name, namespace, inside_labels, False)
+        self.inside_labels = inside_labels
+        self.namespace = namespace
         self.rules = {}
 
     def add_rule(self, direction, outside_resource_id, outside_resource_info, port):
         if outside_resource_id not in self.rules:
             self.rules[outside_resource_id] = Rule(direction, outside_resource_info, port)
-        elif port not in self.rules[outside_resource_id].ports:
+        else:
             self.rules[outside_resource_id].add_port(port)
 
     def generate_yaml(self):
         if not self.rules:
             return None
+        template = templates.policy_template(self.name, self.namespace, self.inside_labels, False)
         for pod_id, rule in self.rules.items():
             is_ingress = rule.direction == Direction.INGRESS
             rule_frame = templates.rule_template(is_ingress)
@@ -59,8 +69,42 @@ class Policy:
             pod_selector["podSelector"]["matchLabels"] = rule.outside_resource_labels
             for port in rule.ports:
                 rule_frame["ports"].append({"port": port})
-            self.template["spec"]["ingress" if is_ingress else "egress"].append(rule_frame)
-        return yaml.dump(self.template)
+            template["spec"]["ingress" if is_ingress else "egress"].append(rule_frame)
+        return yaml.dump(template)
+
+    def merge(self, other_policy):
+        if self.name != other_policy.name:
+            raise ValueError("Cannot merge policies with different names")
+        for rule_id, rule in other_policy.rules.items():
+            if rule_id in self.rules:
+                for port in rule.ports:
+                    self.rules[rule_id].add_port(port)
+            else:
+                self.rules[rule_id] = rule
+
+    @staticmethod
+    def read_from_dict(policy_dict):
+        metadata = policy_dict["metadata"]
+        spec = policy_dict["spec"]
+        policy_name = metadata["name"]
+        resource_type = ResourceType[(policy_name.split("-")[0]).upper()]
+        policy = Policy("", spec["podSelector"]["matchLabels"], metadata["namespace"], resource_type)
+        policy.name = policy_name
+        for rule in spec["ingress"]:
+            for pod in rule["from"]:
+                pod_labels = pod["podSelector"]["matchLabels"]
+                pod_ns = pod["namespaceSelector"]["matchLabels"][K8S_NS_LABEL]
+                for port in rule["ports"]:
+                    policy.add_rule(Direction.INGRESS, policy_name, ResourceInfo("", pod_labels, pod_ns),
+                                    port["port"])
+        for rule in spec["egress"]:
+            for pod in rule["to"]:
+                pod_labels = pod["podSelector"]["matchLabels"]
+                pod_ns = pod["namespaceSelector"]["matchLabels"][K8S_NS_LABEL]
+                for port in rule["ports"]:
+                    policy.add_rule(Direction.EGRESS, policy_name, ResourceInfo("", pod_labels, pod_ns),
+                                    port["port"])
+        return policy
 
 
 class Direction(Enum):
@@ -82,11 +126,16 @@ class Rule:
         self.ports = [port]
 
     def add_port(self, port):
-        self.ports.append(port)
+        if port not in self.ports:
+            self.ports.append(port)
 
     def __eq__(self, other):
         return self.direction == other.direction and self.outside_resource_labels == other.outside_resource_labels and \
                self.outside_resource_ns == other.outside_resource_ns and self.ports == other.ports
+
+    def rule_condition_matched(self, other_rule):
+        return label_matched(self.outside_resource_labels, other_rule.outside_resource_labels) and \
+               self.outside_resource_ns == other_rule.outside_resource_ns
 
 
 def get_port_from_enp_id(enp_id):
@@ -117,6 +166,42 @@ def parse_label(latest_info):
 def assemble_proc_id(host_node_id, pid):
     host_id = host_node_id.split(";")[0]
     return host_id + ";" + pid
+
+
+def compare(static_policy_dicts, dynamic_policies):
+    static_policies = {}
+    for policy_dict in static_policy_dicts:
+        policy = Policy.read_from_dict(policy_dict)
+        static_policies[policy.name] = policy
+
+    abnormal_policies = []
+
+    for policy_name, policy in dynamic_policies.items():
+        if not policy.rules:
+            continue
+        if policy_name not in static_policies:
+            abnormal_policies.append(policy)
+            continue
+        static_policy = static_policies[policy_name]
+        abnormal_policy = copy.deepcopy(policy)
+        abnormal_policy.rules = {}
+        for dynamic_rule in policy.rules.values():
+            matched_rule = None
+            for static_rule in static_policy.rules.values():
+                if dynamic_rule.rule_condition_matched(static_rule):
+                    matched_rule = static_rule
+                    break
+            if matched_rule is None:
+                abnormal_policy.rules[hash(str(dynamic_rule.outside_resource_labels))] = dynamic_rule
+                continue
+            abnormal_rule = copy.deepcopy(dynamic_rule)
+            abnormal_rule.ports = [port for port in dynamic_rule.ports if port not in matched_rule.ports]
+            if abnormal_rule.ports:
+                abnormal_policy.rules[hash(str(dynamic_rule.outside_resource_labels))] = abnormal_rule
+        if abnormal_policy.rules:
+            abnormal_policies.append(abnormal_policy)
+
+    return abnormal_policies
 
 
 def analyze_report(report, uuid):
@@ -190,26 +275,49 @@ def analyze_report(report, uuid):
             policies[adj_rsc_id].add_rule(Direction.INGRESS, rsc_id, resource_id_to_info[rsc_id],
                                           get_port_from_enp_id(adj_enp_id))
 
-    policy_yamls = {}
+    aggregated_policies = {}
     for policy in policies.values():
-        if policy.rules:
-            policy_yamls[policy.name] = policy.generate_yaml()
+        if policy.name in aggregated_policies:
+            aggregated_policies[policy.name].merge(policy)
+        else:
+            aggregated_policies[policy.name] = policy
+    return aggregated_policies
 
-    return policy_yamls
+
+def output_policies_to_dir(policies, dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    # delete all files in the folder
+    for file in os.listdir(dir_path):
+        os.remove(os.path.join(dir_path, file))
+    for policy in policies:
+        yaml_content = policy.generate_yaml()
+        if yaml_content is not None:
+            with open(f"{dir_path}/" + policy.name + ".yaml", "w") as f:
+                f.write(yaml_content)
+
+
+def generate_dynamic_policies(report, uuid):
+    policies = analyze_report(report, uuid)
+    output_policies_to_dir(policies.values(), "policies")
+
+
+def detect_abnormal_conn(static_policy_dicts, report, uuid):
+    policies = analyze_report(report, uuid)
+    abnormal_policies = compare(static_policy_dicts, policies)
+    output_policies_to_dir(abnormal_policies, "abnormal_policies")
 
 
 if __name__ == "__main__":
     debug = False
     start_time = time.time()
+    TEST_DATA_DIR = os.path.abspath(os.path.join(__file__, os.pardir, "test_data"))
     # create folder named "policies" if not exist
-    if not os.path.exists("policies"):
-        os.makedirs("policies")
-    # delete all files in the folder
-    for file in os.listdir("policies"):
-        os.remove(os.path.join("policies", file))
-    test_report = json.load(open("report.json"))
-    files = analyze_report(test_report, "33d1901faed141cf8ccacf5e94961607")
-    for name, content in files.items():
-        with open("policies/" + name + ".yaml", "w") as f:
-            f.write(content)
+    test_uuid = "33d1901faed141cf8ccacf5e94961607"
+    test_report = json.load(open(f"{TEST_DATA_DIR}/report.json"))
+    test_static_policies = []
+    for test_static_file in os.listdir(f"{TEST_DATA_DIR}/static_policies"):
+        test_static_policies.append(
+            yaml.safe_load(open(os.path.join(f"{TEST_DATA_DIR}/static_policies", test_static_file))))
+    detect_abnormal_conn(test_static_policies, test_report, test_uuid)
     print("Time used: " + str(time.time() - start_time))
